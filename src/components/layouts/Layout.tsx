62df987b0233558965b4315973a4f93069cd68ed
Genaba <schalkwykr24@gmail.com>
2025-11-22 18:31:31 +0200

    feat: added dark & light mode switch

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import { ModeToggle } from "../mode-toggle";
 
 export default function Layout() {
   return (
     <>
-      <nav className="sticky top-0 bg-linear-to-r from-[#161EE9]/40 via-[#7816E9]/40 to-[#1687E9]/40 py-2.5 text-center shadow-sm backdrop-blur-xs">
+      <nav className="sticky top-0 flex justify-between bg-linear-to-r p-2.5 text-center shadow-sm outline-1 backdrop-blur-sm">
         <h1 className="text-3xl font-extrabold">Vat Calculator</h1>
+
+        <ModeToggle />
       </nav>
 
       <div className="mx-auto max-w-4xl px-5">
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./components/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="light">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <App />
     </ThemeProvider>
   </StrictMode>,
