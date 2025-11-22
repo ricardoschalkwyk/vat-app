c1d173575733e05a5b6e7a32bc1e4f9e70f2e216
Genaba <schalkwykr24@gmail.com>
2025-11-22 16:58:07 +0200

    chore: updated schema validation rules

--- src/schemas/addProductSchema.ts
+++ src/schemas/addProductSchema.ts
@@ -2,18 +2,19 @@ import * as z from "zod";
 
 export const addProductSchema = z.object({
   id: z.number(),
-  name: z.string().min(1, { message: "Cannot be empty" }),
+  name: z.string().min(1, { message: "Product must have a name" }),
   category: z.string().min(1, { message: "Must have a selected category" }),
   quantity: z
     .string()
-    .regex(/^\d+(?:[.,]\d+)?$/, "Only round numbers allowed")
-    .refine((val) => parseFloat(val) > 0, {
+    .regex(/^\d+$/, "Only round numbers allowed")
+    .refine((val) => parseInt(val, 10) > 0, {
       message: "Value must be greater than 0",
     }),
   priceExclVat: z
     .string()
     .regex(/^\d+(?:[.,]\d+)?$/, "Only digits or decimals are allowed")
-    .refine((val) => parseFloat(val.replace(",", ".")) > 0, {
+    .transform((val: string) => val.replace(",", "."))
+    .refine((val) => parseFloat(val) > 0, {
       message: "Value must be greater than 0",
     }),
   priceInclVat: z.string(),
